33a793e8c2d5f544a6fbc5d237a033941c120272
David Aznaurov <daznauro@redhat.com>
2026-03-31 15:11:32 -0400

    FCN-241: useItem commit

--- packages/react-form-wizard/src/contexts/ItemContext.tsx
+++ packages/react-form-wizard/src/contexts/ItemContext.tsx
@@ -6,7 +6,9 @@ import get from "get-value";
 export const ItemContext = createContext({});
 ItemContext.displayName = "ItemContext";
 
-export function useItem<T = object>(path?: string) {
+export function useItem<T = object>(): T;
+export function useItem<T = object>(path: string): unknown;
+export function useItem<T = object>(path?: string): T | unknown {
   const item = useContext(ItemContext);
   if (path) {
     return get(item, path);
