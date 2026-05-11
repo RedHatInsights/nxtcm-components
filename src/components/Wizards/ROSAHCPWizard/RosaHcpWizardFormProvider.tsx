df87de7332cf73b0e72fffb5b34690e08345b4de
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2026-05-11 12:51:30 -0400

    Connecting yup to rhf
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

--- src/components/Wizards/ROSAHCPWizard/RosaHcpWizardFormProvider.tsx
+++ src/components/Wizards/ROSAHCPWizard/RosaHcpWizardFormProvider.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import React, { useCallback } from 'react';
+import { FormProvider, useForm, type Resolver } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
+import type { ClusterFormData } from '../types';
+
+import { buildClusterValidationSchemaContext } from './buildClusterValidationSchemaContext';
 import { ROSAHCPWizardBody } from './ROSAHCPWizardBody';
-import { getClusterValidationSchemaDefaultValues } from './yupSchemas';
-import type { RosaHCPWizardProps, ROSAHCPCluster } from './types';
+import { useRosaHcpWizardValidators } from './stringsProvider/RosaHcpWizardStringsContext';
+import type { RosaHCPWizardProps } from './types';
+import type { ValidationSchemaContext } from './yupSchemas/types';
+import { clusterValidationSchema, getClusterValidationSchemaDefaultValues } from './yupSchemas';
 
 export function RosaHcpWizardFormProvider(props: RosaHCPWizardProps) {
-  const methods = useForm<Partial<ROSAHCPCluster>>({
-    defaultValues: getClusterValidationSchemaDefaultValues() as Partial<ROSAHCPCluster>,
+  const msgs = useRosaHcpWizardValidators();
+  const { wizardData } = props;
+
+  const resolver = useCallback<Resolver<Partial<ClusterFormData>>>(
+    async (values, context, options) => {
+      const validate = yupResolver(clusterValidationSchema, {
+        context: buildClusterValidationSchemaContext(values, msgs, {
+          checkClusterNameUniqueness: wizardData.checkClusterNameUniqueness as
+            | ValidationSchemaContext['checkClusterNameUniqueness']
+            | undefined,
+        }),
+      }) as Resolver<Partial<ClusterFormData>>;
+      return validate(values, context, options);
+    },
+    [msgs, wizardData.checkClusterNameUniqueness]
+  );
+
+  const methods = useForm<Partial<ClusterFormData>>({
+    defaultValues: getClusterValidationSchemaDefaultValues(),
+    resolver,
     mode: 'onTouched',
   });
 
