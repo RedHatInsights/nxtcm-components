4d258b4610fe7fd9e0248b0df7da2a18a5690553
David Aznaurov <daznauro@redhat.com>
2026-05-12 12:20:48 -0400

    FCN-375: fixed typo in strings

---
+++ src/components/Wizards/ROSAHCPWizard/Steps/BasicSetup/RolesAndPolicies/useUpdateOperatorPrefix.ts
@@ -0,0 +1,14 @@
+import { useFormContext, useWatch } from 'react-hook-form';
+import { ROSAHCPCluster } from '../../../types';
+import { createOperatorRolesPrefix } from '../../../helpers';
+import React from 'react';
+
+export const useUpdateOperatorPrefix = () => {
+  const { setValue } = useFormContext<ROSAHCPCluster>();
+  const clusterName = useWatch<Pick<ROSAHCPCluster, 'name'>>({ name: 'name' });
+
+  const operatorRolesPrefix = createOperatorRolesPrefix(clusterName);
+  React.useEffect(() => {
+    setValue('custom_operator_roles_prefix', operatorRolesPrefix);
+  }, [operatorRolesPrefix, setValue]);
+};
