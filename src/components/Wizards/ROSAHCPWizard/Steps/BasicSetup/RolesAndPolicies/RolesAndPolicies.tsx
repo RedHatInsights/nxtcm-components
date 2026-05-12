651ee27c804886610e9fe5c60244cea37b26cf2d
David Aznaurov <daznauro@redhat.com>
2026-05-12 12:26:12 -0400

    FCN-375: roles and policies step

--- src/components/Wizards/ROSAHCPWizard/Steps/BasicSetup/RolesAndPolicies/RolesAndPolicies.tsx
+++ src/components/Wizards/ROSAHCPWizard/Steps/BasicSetup/RolesAndPolicies/RolesAndPolicies.tsx
@@ -8,16 +8,19 @@ import {
 } from '@patternfly/react-core';
 import { Section } from '../../../components/Section';
 import { useRosaHcpWizardStrings } from '../../../stringsProvider/RosaHcpWizardStringsContext';
-import semver from 'semver';
-import { FieldWithAPIErrorAlert } from '../../../components/FieldWithAPIErrorAlert';
 import React from 'react';
 import PopoverHintWithTitle from '../../../components/PopoverHintWithTitle';
 import { OIDCConfigHint } from '../../../components/OIDCConfigHint';
-import { useFormContext } from 'react-hook-form';
+import { useWatch } from 'react-hook-form';
 import { WizSelect } from '../../../components/WizFields/WizSelect';
 import ExternalLink from '@/components/Wizards/RosaWizard/common/ExternalLink';
 import links from '../../../links';
 import { ROSAHCPCluster, ROSAHCPWizardData } from '../../../types';
+import { useDependentRoles } from './useUpdateRoles';
+import { clusterValidationSchema } from '../../../yupSchemas';
+import { WizTextInput } from '../../../components/WizFields/WizTextInput';
+import { useUpdateOperatorPrefix } from './useUpdateOperatorPrefix';
+import { useInstallerRoleOptions } from './useInstallerRoleOptions';
 
 type RolesAndPoliciesStepProps = Pick<ROSAHCPWizardData, 'roles' | 'oidcConfig'>;
 
@@ -27,56 +30,13 @@ export const RolesAndPolicies = (props: RolesAndPoliciesStepProps) => {
   const [isOperatorRolesOpen, setIsOperatorRolesOpen] = React.useState<boolean>(true);
   const rp = useRosaHcpWizardStrings().rolesAndPolicies;
 
-  const { control, watch, setValue, getValues } = useFormContext<ROSAHCPCluster>();
+  const awsInfrastructureAccount = useWatch<Pick<ROSAHCPCluster, 'associated_aws_id'>>({
+    name: 'associated_aws_id',
+  });
 
-  const selectedClusterVersion = getValues('cluster_version');
-  const awsInfrastructureAccount = getValues('associated_aws_id');
-
-  const installerRoleOptions = React.useMemo(() => {
-    const clusterVer =
-      selectedClusterVersion && semver.valid(semver.coerce(selectedClusterVersion));
-    return roles.data.map((r) => {
-      const role = r.installerRole;
-      if (!role.roleVersion || !clusterVer) {
-        return role;
-      }
-      const roleVer = semver.valid(semver.coerce(role.roleVersion));
-      const disabled = roleVer != null && semver.lt(roleVer, clusterVer);
-      return disabled
-        ? {
-            ...role,
-            ariaDisabled: true,
-            tooltipProps: { content: rp.installerRoleOptionDisabledDescription },
-          }
-        : { ...role };
-    });
-  }, [roles, selectedClusterVersion, rp.installerRoleOptionDisabledDescription]);
-
-  const selectedInstallerArn = watch('installer_role_arn');
-  const selectedRole = React.useMemo(
-    () => roles.data.find((r) => r.installerRole.value === selectedInstallerArn),
-    [roles, selectedInstallerArn]
-  );
-  const supportRoleOptions = selectedRole?.supportRole ?? [];
-  const workerRoleOptions = selectedRole?.workerRole ?? [];
-
-  React.useEffect(() => {
-    const subscription = watch((formValues, { name }) => {
-      if (name !== 'installer_role_arn') return;
-      const installerValue = formValues.installer_role_arn;
-      if (!installerValue) {
-        setValue('support_role_arn', '');
-        setValue('worker_role_arn', '');
-        return;
-      }
-      const role = roles.data.find((r) => r.installerRole.value === installerValue);
-      if (role) {
-        setValue('support_role_arn', role.supportRole[0]?.value ?? '');
-        setValue('worker_role_arn', role.workerRole[0]?.value ?? '');
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, [watch, roles, setValue]);
+  const installerRoleOptions = useInstallerRoleOptions(roles);
+  const { supportRoleOptions, workerRoleOptions } = useDependentRoles(roles);
+  useUpdateOperatorPrefix();
 
   const rosaCommand = `rosa create operator-roles --prefix "custom-operator-roles-prefix" --oidc-config-id "byo-oidc-config-id" --hosted-cp --installer-role-arn "installer-role-arn`;
 
@@ -85,60 +45,45 @@ export const RolesAndPolicies = (props: RolesAndPoliciesStepProps) => {
       <Section label={rp.accountRolesSection}>
         <Grid>
           <GridItem span={7}>
-            <FieldWithAPIErrorAlert
-              error={roles.error}
-              isFetching={roles.isFetching}
-              fieldName={rp.installerRoleLabel}
-            >
-              <WizSelect
-                isRequired
-                onRefresh={() => void roles.fetch(awsInfrastructureAccount ?? '')}
-                labelHelp={
-                  <>
-                    {rp.installerHelpLead}{' '}
-                    <ExternalLink href={links.ROSA_ROLES_LEARN_MORE}>
-                      {rp.installerLearnMoreLink}
-                    </ExternalLink>
-                  </>
-                }
-                label={rp.installerRoleLabel}
-                name="installer_role_arn"
-                keyPath="installer_role_arn"
-                control={control}
-                placeholder={rp.installerPlaceholder}
-                options={installerRoleOptions}
-              />
-            </FieldWithAPIErrorAlert>
+            <WizSelect<ROSAHCPCluster>
+              schema={clusterValidationSchema}
+              apiError={roles.error}
+              isLoading={roles.isFetching}
+              onRefresh={() => void roles.fetch(awsInfrastructureAccount ?? '')}
+              labelHelp={
+                <>
+                  {rp.installerHelpLead}{' '}
+                  <ExternalLink href={links.ROSA_ROLES_LEARN_MORE}>
+                    {rp.installerLearnMoreLink}
+                  </ExternalLink>
+                </>
+              }
+              name="installer_role_arn"
+              options={installerRoleOptions}
+            />
           </GridItem>
         </Grid>
         <ExpandableSection
           isExpanded={isArnsOpen}
           onToggle={() => setIsArnsOpen(!isArnsOpen)}
           toggleText={rp.arnsToggle}
+          className="pf-v6-u-mb-lg"
         >
           <Grid hasGutter>
             <GridItem span={7}>
-              <WizSelect
+              <WizSelect<ROSAHCPCluster>
                 isRequired
+                schema={clusterValidationSchema}
                 name="support_role_arn"
-                label={rp.supportRoleLabel}
-                keyPath="support_role_arn"
-                control={control}
-                labelHelp={rp.supportHelp}
-                placeholder={rp.supportPlaceholder}
                 options={supportRoleOptions}
                 isDisabled
               />
             </GridItem>
             <GridItem span={7}>
-              <WizSelect
+              <WizSelect<ROSAHCPCluster>
                 isRequired
+                schema={clusterValidationSchema}
                 name="worker_role_arn"
-                label={rp.workerRoleLabel}
-                keyPath="worker_role_arn"
-                control={control}
-                labelHelp={rp.workerHelp}
-                placeholder={rp.workerPlaceholder}
                 options={workerRoleOptions}
                 isDisabled
               />
@@ -151,23 +96,15 @@ export const RolesAndPolicies = (props: RolesAndPoliciesStepProps) => {
           <GridItem span={7}>
             <Stack>
               <StackItem>
-                <FieldWithAPIErrorAlert
-                  error={oidcConfig.error}
-                  isFetching={oidcConfig.isFetching}
-                  fieldName={rp.oidcLabel}
-                >
-                  <WizSelect
-                    onRefresh={oidcConfig.fetch}
-                    control={control}
-                    name="byo_oidc_config_id"
-                    label={rp.oidcLabel}
-                    isRequired
-                    labelHelp={rp.oidcHelp}
-                    placeholder={rp.oidcPlaceholder}
-                    labelHelpTitle={rp.oidcPopoverTitle}
-                    options={oidcConfig.data}
-                  />
-                </FieldWithAPIErrorAlert>
+                <WizSelect<ROSAHCPCluster>
+                  onRefresh={oidcConfig.fetch}
+                  apiError={oidcConfig.error}
+                  isLoading={oidcConfig.isFetching}
+                  schema={clusterValidationSchema}
+                  name="byo_oidc_config_id"
+                  isRequired
+                  options={oidcConfig.data}
+                />
               </StackItem>
               <StackItem>
                 <PopoverHintWithTitle
@@ -186,7 +123,22 @@ export const RolesAndPolicies = (props: RolesAndPoliciesStepProps) => {
           toggleText={rp.operatorPrefixToggle}
         >
           <Grid>
-            <GridItem span={4}>custom_operator_roles_prefix</GridItem>
+            <GridItem span={4}>
+              <WizTextInput<ROSAHCPCluster>
+                name="custom_operator_roles_prefix"
+                schema={clusterValidationSchema}
+                label={rp.operatorPrefixLabel}
+                labelHelp={
+                  <>
+                    {rp.operatorPrefixHelpLead}{' '}
+                    <ExternalLink href={links.ROSA_OIDC_LEARN_MORE}>
+                      {rp.operatorPrefixLearnMoreLink}
+                    </ExternalLink>
+                  </>
+                }
+                helperText={rp.operatorPrefixHelper}
+              />
+            </GridItem>
           </Grid>
           <ClipboardCopy
             variant="expansion"
