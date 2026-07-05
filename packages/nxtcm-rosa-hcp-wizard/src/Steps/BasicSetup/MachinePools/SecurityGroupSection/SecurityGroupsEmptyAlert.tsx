b6a42c3b412cf34addfbb41e5090b1326dc6b08d
Kim Doberstein <kdoberst@redhat.com>
2026-07-05 14:07:22 -0500

    FCN-609 add security groups refresh

--- packages/nxtcm-rosa-hcp-wizard/src/Steps/BasicSetup/MachinePools/SecurityGroupSection/SecurityGroupsEmptyAlert.tsx
+++ packages/nxtcm-rosa-hcp-wizard/src/Steps/BasicSetup/MachinePools/SecurityGroupSection/SecurityGroupsEmptyAlert.tsx
@@ -1,22 +1,33 @@
-import { Alert, AlertActionLink } from '@patternfly/react-core';
+import { Alert, Stack, StackItem } from '@patternfly/react-core';
 import ExternalLink from '../../../../components/ExternalLink';
 import links from '../../../../constants/links';
 import { useRosaHcpWizardStrings } from '../../../../stringsProvider/RosaHcpWizardStringsContext';
+import { SecurityGroupsRefreshButton } from './SecurityGroupsRefreshButton';
 
 type SecurityGroupsEmptyAlertProps = {
   refreshVPCCallback?: () => void;
+  isVPCLoading?: boolean;
 };
-const SecurityGroupsEmptyAlert = ({ refreshVPCCallback }: SecurityGroupsEmptyAlertProps) => {
+const SecurityGroupsEmptyAlert = ({
+  refreshVPCCallback,
+  isVPCLoading,
+}: SecurityGroupsEmptyAlertProps) => {
   const sg = useRosaHcpWizardStrings().securityGroups;
   return (
-    <Alert variant="info" isInline title={sg.emptyTitle}>
-      {sg.emptyBodyPrefix}{' '}
-      <ExternalLink href={links.AWS_CONSOLE_SECURITY_GROUPS}>{sg.emptyConsoleLink}</ExternalLink>{' '}
-      {sg.emptyBodySuffix} <br />
-      {refreshVPCCallback && (
-        <AlertActionLink onClick={refreshVPCCallback}>{sg.refreshLink}</AlertActionLink>
-      )}
-    </Alert>
+    <Stack hasGutter>
+      <StackItem>
+        <Alert variant="info" isInline title={sg.emptyTitle}>
+          {sg.emptyBodyPrefix}{' '}
+          <ExternalLink href={links.AWS_CONSOLE_SECURITY_GROUPS}>
+            {sg.emptyConsoleLink}
+          </ExternalLink>{' '}
+          {sg.emptyBodySuffix}
+        </Alert>
+      </StackItem>
+      <StackItem>
+        <SecurityGroupsRefreshButton onRefresh={refreshVPCCallback} isLoading={isVPCLoading} />
+      </StackItem>
+    </Stack>
   );
 };
 
