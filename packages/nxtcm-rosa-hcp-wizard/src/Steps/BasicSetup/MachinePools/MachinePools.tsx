9c0eb7fe721405d50804ba7cc67847cab0a2f216
David Aznaurov <daznauro@redhat.com>
2026-08-13 16:20:29 -0400

    FCN-686: added memoization to sorting func for perf

--- packages/nxtcm-rosa-hcp-wizard/src/Steps/BasicSetup/MachinePools/MachinePools.tsx
+++ packages/nxtcm-rosa-hcp-wizard/src/Steps/BasicSetup/MachinePools/MachinePools.tsx
@@ -27,11 +27,13 @@ export const MachinePools = (props: MachinePoolsProps) => {
 
   const { control } = useFormContext<Partial<ROSAHCPCluster>>();
 
-  const generalPurposeMachineTypesFirst = [...machineTypes.data].sort((a, b) => {
-    const aIsGP = a?.category?.includes('General Purpose') ? 1 : 0;
-    const bIsGP = b?.category?.includes('General Purpose') ? 1 : 0;
-    return bIsGP - aIsGP;
-  });
+  const generalPurposeMachineTypesFirst = useMemo(() => {
+    return [...machineTypes.data].sort((a, b) => {
+      const aIsGP = a?.category?.includes('General Purpose') ? 1 : 0;
+      const bIsGP = b?.category?.includes('General Purpose') ? 1 : 0;
+      return bIsGP - aIsGP;
+    });
+  }, [machineTypes.data]);
 
   const region = useWatch({ control, name: 'region' });
   const clusterVersion = useWatch({ control, name: 'cluster_version' }) ?? '';
